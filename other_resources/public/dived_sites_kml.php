decef1d1a63b3a3c52f1c8620393ce1bfa97f22b
Timothy Clarke <Timothy.Clarke@ServerBox.co.uk>
2018-05-23 17:04:41 +0100

    Maps integration wording update 'Dives' -> 'Diver Dives'

--- other_resources/public/dived_sites_kml.php
+++ other_resources/public/dived_sites_kml.php
@@ -13,7 +13,7 @@ $sql_data = $conn->query($sql_select);
 if ($sql_data->num_rows > 0) {
   echo '<?xml version="1.0" encoding="UTF-8"?><kml xmlns="http://www.opengis.net/kml/2.2"><Document>';
   while($row = $sql_data->fetch_assoc()) {
-    echo '<Placemark><name><![CDATA[ ' . $row['site_name'] . ' ]]></name><description><![CDATA[ ' . $row['dives'] . ' Dives ]]></description><Point><coordinates> ' . $row['east'] . ',' . $row['north'] . '</coordinates></Point></Placemark>';
+    echo '<Placemark><name><![CDATA[ ' . $row['site_name'] . ' ]]></name><description><![CDATA[ ' . $row['dives'] . ' Diver Dives ]]></description><Point><coordinates> ' . $row['east'] . ',' . $row['north'] . '</coordinates></Point></Placemark>';
 	}
 }
 $conn->close();
