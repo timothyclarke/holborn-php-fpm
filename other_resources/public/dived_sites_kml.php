fc74300e216b5df6464472b8d86d7f6471ce60e7
Timothy Clarke <Timothy.Clarke@ServerBox.co.uk>
2018-05-23 17:33:33 +0100

    Diver Dives with gps was giving too many results if there was more than one set of gps locations

--- other_resources/public/dived_sites_kml.php
+++ other_resources/public/dived_sites_kml.php
@@ -7,13 +7,19 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql_select = "select site_name,north,east,count(dive_id) as dives from diver_dives natural join dives natural join sites natural join sites_gps group by site_id";
+$get_diver_dives = "select site_id,site_name,count(dive_id) as dives from diver_dives natural join dives natural join sites group by site_id"
 
-$sql_data = $conn->query($sql_select);
-if ($sql_data->num_rows > 0) {
+$dive_data = $conn->query($get_diver_dives);
+if ($dive_data->num_rows > 0) {
   echo '<?xml version="1.0" encoding="UTF-8"?><kml xmlns="http://www.opengis.net/kml/2.2"><Document>';
-  while($row = $sql_data->fetch_assoc()) {
-    echo '<Placemark><name><![CDATA[ ' . $row['site_name'] . ' ]]></name><description><![CDATA[ ' . $row['dives'] . ' Diver Dives ]]></description><Point><coordinates> ' . $row['east'] . ',' . $row['north'] . '</coordinates></Point></Placemark>';
+  while($dive_row = $sql_data->fetch_assoc()) {
+    $gps_location = "select north,east from sites_gps where site_id = " . $dive_row['site_id'] ." order by gps_id limit 1";
+    $gps_data = $conn->query($gps_location);
+    if ($gps_data->num_rows > 0) {
+      while($gps_row = $gps_data->fetch_assoc()) {
+        echo '<Placemark><name><![CDATA[ ' . $dive_row['site_name'] . ' ]]></name><description><![CDATA[ ' . $dive_row['dives'] . ' Diver Dives ]]></description><Point><coordinates> ' . $gps_row['east'] . ',' . $gps_row['north'] . '</coordinates></Point></Placemark>';
+	    }
+	  }
 	}
 }
 $conn->close();
