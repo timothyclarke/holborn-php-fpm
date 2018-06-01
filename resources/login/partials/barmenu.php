53804840bb9129a5eba1669362fe9e01580abbc1
Timothy Clarke <Timothy.Clarke@ServerBox.co.uk>
2018-06-01 14:24:38 +0100

    Updated top menu

--- resources/login/partials/barmenu.php
+++ resources/login/partials/barmenu.php
@@ -2,12 +2,12 @@
 
 // Define the buttons in the menu bar
 $barmenu = array(
-  "index"     => "index.php",
-  "New Dive"  => "new_dive.php",
-  "Other Pages" => array(
-    "Main Page"     =>  "/",
-    "Diving Stats"  => "diving_stats.php",
-    "All Sites"     => "all_sites.php",
-    "Dived Sites"   => "dived_sites.php"
+  "New Dive Form" => "new_dive.php",
+  "Other Pages"   => array(
+    "Main Holborn Page" => "/",
+    "Diving Stats"      => "diving_stats.php",
+    "All Sites"         => "all_sites.php",
+    "Dived Sites"       => "dived_sites.php",
+    "Resources Home"    => "/resources/"
   ),
 );
